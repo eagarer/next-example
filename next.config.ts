6bb40873be3561588e82e5dda0ce018a8f59bc4f
eagarer <erayagarer@gmail.com>
2025-11-03 16:30:44 +0300

    Edited next conf

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   reactStrictMode: false,
-
+  basePath: '/next-example',
+  assetPrefix: '/next-example/',
 };
 
 export default nextConfig;
