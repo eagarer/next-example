261ebb47314838809843b45698246d4362959c69
eagarer <erayagarer@gmail.com>
2025-11-05 16:50:03 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import ContainerB from "@/containers/ContainerB";
 export default function Home() {
   return (
     <div className="p-4">
+      test
       {/* İlk container */}
       <ContainerA />
 
